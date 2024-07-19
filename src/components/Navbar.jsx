4c4e99f8470703a800704ce2984b758dbb01ff05
ahmadshaar723 <ahmadshaar723@gmail.com>
2024-07-19 16:29:30 +0300

    last update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,16 +10,16 @@ function Navbar() {
         <img className="mx-2 w-12 rounded-full" src={logo} alt="logo" />
       </div>
       <div className="social">
-        <motion.a whileHover={{scale:1.1}} href="https://www.linkedin.com/in/ahmad-shaar-1594b3209?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">
+        <motion.a whileHover={{scale:1.1}} className="hover:text-slate-400" href="https://www.linkedin.com/in/ahmad-shaar-1594b3209?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">
           <FaLinkedin />
         </motion.a>
-        <motion.a whileHover={{scale:1.1}} href="https://github.com/" target="_blank">
+        <motion.a whileHover={{scale:1.1}} className="hover:text-slate-400" href="https://github.com/" target="_blank">
           <FaGithub />
         </motion.a>
-        <motion.a whileHover={{scale:1.1}} href="https://www.facebook.com/ahmad.shaar.9674?mibextid=ZbWKwL" target="_blank">
+        <motion.a whileHover={{scale:1.1}} className="hover:text-slate-400" href="https://www.facebook.com/ahmad.shaar.9674?mibextid=ZbWKwL" target="_blank">
           <FaFacebook />
         </motion.a>
-        <motion.a whileHover={{scale:1.1}} href="https://www.instagram.com/shaar_ahmad?igsh=MWs2dzl6eGlmaXgzNQ==" target="_blank">
+        <motion.a whileHover={{scale:1.1}} className="hover:text-slate-400" href="https://www.instagram.com/shaar_ahmad?igsh=MWs2dzl6eGlmaXgzNQ==" target="_blank">
           <FaInstagram />
         </motion.a>
       </div>
